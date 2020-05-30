971f59e729f47e67c4d35307fa58f9493d536991
josue <josuevelezdr@gmail.com>
2020-05-30 12:00:39 +0200

    Fix parser with valid pattern matching

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -6,8 +6,7 @@ defmodule Servy.Parser do
     [request_line | header_lines] = String.split(top, "\n")
     [method, path, _] = String.split(request_line, " ")
     parsed_headers = parse_headers(header_lines, %{})
-    params_string = parse_params(parsed_headers["Content-type"], params_string)
-
+    params_string = parse_params(parsed_headers, params_string)
     %Conv{
       method: method,
       path: path,
@@ -16,20 +15,19 @@ defmodule Servy.Parser do
     }
   end
 
-  #just handled this kind of content type
-  def parse_params("application/x-www-form-urlencoded", params_string) do
-    params_string |> String.trim |> URI.decode_query
-  end
-
-  def parse_params(_, _), do: %{}
-
   def parse_headers([head | tail], headers) do
-    IO.inspect "PARSE HEADERS"
-    IO.inspect head
     [key, value] = String.split(head, ": ")
     headers = Map.put(headers, key, value)
     parse_headers(tail, headers)
   end
 
   def parse_headers([], headers), do: headers
+
+  #just handled this kind of content type
+
+  def parse_params(%{"Content-Type" => "application/x-www-form-urlencoded"} = headers, params_string) do
+    params_string |> String.trim |> URI.decode_query
+  end
+
+  def parse_params(_, _), do: %{}
 end
